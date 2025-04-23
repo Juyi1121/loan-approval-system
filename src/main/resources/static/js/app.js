493bf899673be7702b4598e6c41d907a4b005b59
Joy <joy@example.com>
2025-04-24 00:06:58 +0800

    描述你這次更新的內容

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+// Bootstrap 5 前端驗證
+(() => {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+  
